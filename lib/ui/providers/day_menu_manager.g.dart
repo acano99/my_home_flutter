cc5ea1b20492c271572fff58f6dd73973125181a
acano <abrahan.jcano99@gmail.com>
2025-03-20 16:04:18 -0400

    provider para la gestion del daymenu

---
+++ lib/ui/providers/day_menu_manager.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'day_menu_manager.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dayMenuManagerHash() => r'3836fd1cc62650982d6c8e4136f076bc5583e547';
+
+/// See also [DayMenuManager].
+@ProviderFor(DayMenuManager)
+final dayMenuManagerProvider =
+    AutoDisposeAsyncNotifierProvider<DayMenuManager, DayMenu>.internal(
+      DayMenuManager.new,
+      name: r'dayMenuManagerProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$dayMenuManagerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$DayMenuManager = AutoDisposeAsyncNotifier<DayMenu>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
