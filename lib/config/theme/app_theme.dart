0db1e3db24ff6b86373f65f7646b9ffec84c6140
acano <abrahan.jcano99@gmail.com>
2025-03-20 20:56:56 -0400

    agregado metodo para cambiar el tema

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+part 'app_theme.g.dart';
+
 @riverpod
-ThemeData app_theme({bool darkTheme = false}) => ThemeData(
-  cardTheme: CardTheme(elevation: 3),
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: Colors.deepPurple,
-    brightness: darkTheme ? Brightness.dark : Brightness.light,
-  ),
-);
+class AppTheme extends _$AppTheme {
+  @override
+  ThemeData build() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      cardTheme: CardTheme(elevation: 3),
+      colorSchemeSeed: Colors.deepPurple,
+    );
+  }
+
+  void switchTheme(bool darkMode) {
+    state = state.copyWith(
+      brightness: darkMode ? Brightness.dark : Brightness.light,
+    );
+  }
+}
