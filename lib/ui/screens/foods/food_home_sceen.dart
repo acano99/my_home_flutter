61e9f174672fd04dd569144bbd6ad92388a2e911
acano <abrahan.jcano99@gmail.com>
2025-03-20 20:57:43 -0400

    actualizacion de la ui

--- lib/ui/screens/foods/food_home_sceen.dart
+++ lib/ui/screens/foods/food_home_sceen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:my_home/config/router/routes.dart';
 import 'package:my_home/data/enums/fab_type.dart';
 import 'package:my_home/ui/screens/common/common_screen.dart';
 import 'package:my_home/ui/widgets/theme/theme_container.dart';
@@ -14,7 +16,12 @@ class FoodHomeSceen extends StatelessWidget {
   Widget build(BuildContext context) {
     return CommonScreen(
       appBarTitle: "Foods",
-      fab: ThemeFab(onPressed: () {}, fabType: FabType.add),
+      fab: ThemeFab(
+        onPressed: () {
+          if (context.mounted) context.goNamed(Routes.foodAdd);
+        },
+        fabType: FabType.add,
+      ),
       childrens: [
         ThemeDatePicker(onTap: () {}),
         SizedBox(height: 8),
