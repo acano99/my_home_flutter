34d1259c682e9952e867333ffb7381462aa674ef
acano <abrahan.jcano99@gmail.com>
2025-03-20 20:57:33 -0400

    cambio de navegacion por un drawer

--- lib/ui/screens/common/common_screen.dart
+++ lib/ui/screens/common/common_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_home/config/theme/app_theme.dart';
+
+import '../../../config/router/routes.dart';
 
 class CommonScreen extends StatelessWidget {
   final String appBarTitle;
@@ -17,10 +20,62 @@ class CommonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Container(width: double.infinity, height: 200, color: Colors.amber),
+            _DrawerButton(
+              icon: Icons.restaurant_menu_rounded,
+              label: "Foods",
+              onTap: () {
+                if (context.mounted) {
+                  context.goNamed(Routes.foodHome);
+                }
+              },
+            ),
+            _DrawerButton(
+              icon: Icons.restaurant_menu_rounded,
+              label: "Foods",
+              onTap: () {
+                if (context.mounted) {
+                  context.goNamed(Routes.foodHome);
+                }
+              },
+            ),
+            _DrawerButton(
+              icon: Icons.restaurant_menu_rounded,
+              label: "Foods",
+              onTap: () {
+                if (context.mounted) {
+                  context.goNamed(Routes.foodHome);
+                }
+              },
+            ),
+            _DrawerButton(
+              icon: Icons.restaurant_menu_rounded,
+              label: "Foods",
+              onTap: () {
+                if (context.mounted) {
+                  context.goNamed(Routes.foodHome);
+                }
+              },
+            ),
+            _DrawerButton(
+              icon: Icons.restaurant_menu_rounded,
+              label: "Foods",
+              onTap: () {
+                if (context.mounted) {
+                  context.goNamed(Routes.foodHome);
+                }
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text(appBarTitle),
         actions: [
-          //_SwitchTheme(),
+          _SwitchTheme(),
           IconButton(onPressed: () {}, icon: Icon(Icons.settings_rounded)),
           SizedBox(width: 8),
         ],
@@ -34,22 +89,39 @@ class CommonScreen extends StatelessWidget {
   }
 }
 
-// class _SwitchTheme extends ConsumerStatefulWidget {
-//   const _SwitchTheme();
+class _DrawerButton extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final VoidCallback onTap;
 
-//   @override
-//   ConsumerState<ConsumerStatefulWidget> createState() => _SwitchThemeState();
-// }
+  const _DrawerButton({
+    required this.label,
+    required this.icon,
+    required this.onTap,
+  });
 
-// class _SwitchThemeState extends ConsumerState<_SwitchTheme> {
-//   var darkMode = false;
-//   @override
-//   Widget build(BuildContext context) {
-//     return IconButton(
-//       onPressed: () {
-//         ref.read(appThemeProvider(darkTheme: false));
-//       },
-//       icon: Icon(Icons.dark_mode_rounded),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(title: Text(label), trailing: Icon(icon), onTap: onTap);
+  }
+}
+
+class _SwitchTheme extends ConsumerStatefulWidget {
+  const _SwitchTheme();
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SwitchThemeState();
+}
+
+class _SwitchThemeState extends ConsumerState<_SwitchTheme> {
+  var darkMode = false;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        ref.read(appThemeProvider.notifier).switchTheme(darkMode);
+      },
+      icon: Icon(Icons.dark_mode_rounded),
+    );
+  }
+}
