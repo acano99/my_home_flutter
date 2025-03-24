0b690523da6cbd88f8a42d05f3dc88e8efe30a9d
acano <abrahan.jcano99@gmail.com>
2025-03-24 09:12:09 -0400

    ajustes en los use case, no hay necesidad de que sean providers

--- lib/domain/usecase/get_day_menu_use_case.dart
+++ lib/domain/usecase/get_day_menu_use_case.dart
@@ -1,11 +1,6 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_home/data/database/app_database.dart';
 import 'package:my_home/data/database/dao/day_menu_dao.dart';
 import 'package:my_home/data/models/day_menu.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'get_day_menu_use_case.g.dart';
-
-@riverpod
-Future<List<DayMenu>> getDayMenuUSeCase(Ref ref, String date) async =>
+Future<List<DayMenu>> getDayMenuUSeCase(String date) async =>
     DayMenuDao(db: await AppDatabase().database).getDayMenu(date);
--- lib/domain/usecase/insert_day_menu_use_case.dart
+++ lib/domain/usecase/insert_day_menu_use_case.dart
@@ -1,11 +1,6 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_home/data/database/dao/day_menu_dao.dart';
 import 'package:my_home/data/models/day_menu.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:my_home/data/database/app_database.dart';
 
-part 'insert_day_menu_use_case.g.dart';
-
-@riverpod
-Future<int> insertDayMenuUseCase(Ref ref, DayMenu dayMenu) async =>
+Future<int> insertDayMenuUseCase(DayMenu dayMenu) async =>
     DayMenuDao(db: await AppDatabase().database).insertDayMenu(dayMenu);
