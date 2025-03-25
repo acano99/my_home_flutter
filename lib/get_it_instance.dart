155931f4187c2b9d312e8f2276e545ee5921598c
acano <abrahan.jcano99@gmail.com>
2025-03-24 23:32:11 -0400

    actualizacion de los blocs

--- lib/get_it_instance.dart
+++ lib/get_it_instance.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:my_home/config/router/app_router.dart';
+import 'package:my_home/config/theme/app_theme.dart';
 
 final GetIt getIt = GetIt.instance;
 
 void getItInit() {
   getIt.registerLazySingleton(() => AppRouterCubit());
+  getIt.registerLazySingleton(() => AppThemeCubit());
 }
