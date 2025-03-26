fd725c428af48a27de3bc990a296253a488d5d23
acano <abrahan.jcano99@gmail.com>
2025-03-26 03:11:39 -0400

    actualizacion de los modelos

--- lib/data/models/day_menu.dart
+++ lib/data/models/day_menu.dart
@@ -1,18 +1,29 @@
+import 'package:equatable/equatable.dart';
 import 'package:my_home/data/models/food.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'day_menu.g.dart';
 
 @JsonSerializable()
-class DayMenu {
-  final int? id = null;
+class DayMenu extends Equatable {
+  final int? id;
   final String date;
   final List<Food> foods;
 
-  DayMenu({required this.date, required this.foods});
+  const DayMenu({this.id, required this.date, required this.foods});
+
+  DayMenu copyWith({
+    final int? id,
+    final String? date,
+    final List<Food>? foods,
+  }) => DayMenu(id: id, date: date ?? this.date, foods: foods ?? this.foods);
 
   factory DayMenu.fromJson(Map<String, dynamic> json) =>
       _$DayMenuFromJson(json);
 
   Map<String, dynamic> toJson() => _$DayMenuToJson(this);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id, date, foods];
 }
--- lib/data/models/food.dart
+++ lib/data/models/food.dart
@@ -1,17 +1,24 @@
+import 'package:equatable/equatable.dart';
 import 'package:my_home/data/enums/food_type.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'food.g.dart';
 
 @JsonSerializable()
-class Food {
-  final int? id = null;
+class Food extends Equatable {
+  final int id;
   final String name;
   final FoodType type;
 
-  Food({required this.name, required this.type});
+  const Food({this.id = 0, required this.name, required this.type});
+
+  Food copyWith({final int? id, final String? name, final FoodType? type}) =>
+      Food(id: id ?? this.id, name: name ?? this.name, type: type ?? this.type);
 
   factory Food.fromJson(Map<String, dynamic> json) => _$FoodFromJson(json);
 
   Map<String, dynamic> toJson() => _$FoodToJson(this);
+
+  @override
+  List<Object?> get props => [id, name, type];
 }
