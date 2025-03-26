cca50c9e6350d8774b96416c0a671a318a53c85b
acano <abrahan.jcano99@gmail.com>
2025-03-26 03:12:25 -0400

    actualizacion del cambio de tema

--- lib/ui/screens/common/common_screen.dart
+++ lib/ui/screens/common/common_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';  
 import 'package:go_router/go_router.dart';
 import 'package:my_home/config/theme/app_theme.dart';
 
